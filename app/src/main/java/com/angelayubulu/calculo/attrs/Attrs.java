c4c15b140abcea98a18dddeee7a57b2d3605254b
ANGELA MORENO <ajmd1992@hotmail.com>
2016-05-25 14:23:56 -0500

    Initial commit

---
+++ app/src/main/java/com/angelayubulu/calculo/attrs/Attrs.java
@@ -0,0 +1,7 @@
+package com.angelayubulu.calculo.attrs;
+
+/**
+ * Created by Angela Yubulu on 25/05/2016.
+ */
+public class Attrs {
+}
---
+++ app/src/main/java/com/angelayubulu/calculo/models/Usuario.java
@@ -0,0 +1,7 @@
+package com.angelayubulu.calculo.models;
+
+/**
+ * Created by Angela Yubulu on 25/05/2016.
+ */
+public class Usuario {
+}
